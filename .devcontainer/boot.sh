dea74b48eb5dc23944b804c8c24a2531bc6f7c1e
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2023-04-21 20:19:03 +0545

    Setup devcontainer for vscode users

---
+++ .devcontainer/boot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Setting SSH password for vscode user..."
+sudo usermod --password $(echo vscode | openssl passwd -1 -stdin) vscode
+
+echo "Updating RubyGems..."
+gem update --system -N
+
+echo "Installing dependencies..."
+bundle install
+yarn install
+
+echo "Copying database.yml..."
+# cp -n config/database.yml.example config/database.yml
+
+echo "Setting up the project..."
+bin/setup
+
+echo "Running the project..."
+bin/dev
+
+echo "Done!"
