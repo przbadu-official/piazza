83b7b18f2f001a192f056b2121466a301408980d
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2023-04-22 10:50:21 +0000

    Generate basic UI for login form with routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'feed#show'
 
+  # Register
   get 'sign_up', to: 'users#new'
   post 'sign_up', to: 'users#create'
+  # login
+  get 'login', to: 'sessions#new'
 end
