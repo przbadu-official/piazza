ad0a92faead3583f38cb5bd847ccf51d926bdd08
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2023-04-22 15:17:41 +0000

    Test authentication concern
    * to test authentication concern which can be used in any controller, we create a AuthenticateTests controller
    * We than draw a routes for test environment, which will draw route for testing
    * And we skip authentication from certain test controller action
    * Add a testcase to make sure Authenticate concern is handling authentication logic properly.

---
+++ test/support/routes_helper.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Ensure you call `reload_routes!` in your test's `teardown`
+# to erase al the routes drawn by your test case
+module RoutesHelpers
+  def draw_test_routes(&block)
+    # Don't clear routes when calling `Rails.application.routes.draw`
+    Rails.application.routes.disable_clear_and_finalize = true
+
+    Rails.application.routes.draw do
+      scope('test') do
+        instance_exec(&block)
+      end
+    end
+  end
+
+  def reload_routes!
+    Rails.application.reload_routes!
+  end
+end
